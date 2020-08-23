4dc10d9158e0aac103c463770bc99cb88189a4cb
connorshea15 <connorshea15@gmail.com>
2020-08-23 11:39:15 -0700

    Category routes are complete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,14 +34,35 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbCategoryData => res.json(dbCategoryData));
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then(dbCategoryData => res.json(dbCategoryData));
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+
+  }).then(dbCategoryData => res.json(dbCategoryData));
 });
 
 module.exports = router;
