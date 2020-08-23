77a9425a63aa86e30083878c4659788116a9b3ce
connorshea15 <connorshea15@gmail.com>
2020-08-23 11:29:44 -0700

    I have two working routes with about 10 to go

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,13 +4,32 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock']
+      }
+    ]
+  })
+  .then(dbCategoryData => res.json(dbCategoryData));
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock']
+      }
+    ]
+  })
+  .then(dbCategoryData => res.json(dbCategoryData));
 });
 
 router.post('/', (req, res) => {
