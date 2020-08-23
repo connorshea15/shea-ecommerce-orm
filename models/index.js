5737a19f68c52f9ea59f03710c76ea635c0edcac
connorshea15 <connorshea15@gmail.com>
2020-08-23 11:12:46 -0700

    Models are cooking!

--- models/index.js
+++ models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag
+  //I might need some more info
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag
+  //might need some more here too
+});
 
 module.exports = {
   Product,
